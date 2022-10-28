8d9b0d47a163ebe1ab28876d5f930d6dc04e6433
Kaitomy <99449281+Kaitomy@users.noreply.github.com>
2022-10-28 13:16:58 +0300

    Praktika3

---
+++ flutter_application_5/lib/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_application_5/common/data_base_reguest.dart';
+import 'package:flutter_application_5/core/db/data_base_helper.dart';
+import 'package:flutter_application_5/data/model/users.dart';
+import 'package:flutter_application_5/domain/entity/role_entity.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_application_5/domain/repositories/auth_repositories.dart';
+import 'package:sqflite/sqflite.dart';
+
+class AuthReposityImpl implements AuthRepositories{
+  final _db = DataBaseHelper.instance.dataBase;
+@override
+  // TODO: implement table
+  String get table => DataBaseRequest.tableUsers;
+
+  @override
+  Future<Either<String, RoleEnum>> signIn(String login, String password,String email) async {
+try{var user = (await _db.query(table,where: 'login = ?',whereArgs: [login])).map((e) => Users.toFromMap(e));
+
+if(user.isEmpty)
+{
+return const Left('Такого пользователя нет');
+}
+if(user.first.password != password)
+{
+return const Left('Неправильный пароль');
+}
+
+return Right(user.first.id_role);
+} on DatabaseException catch (error) { return const Left('');}
+  }
+
+
+  @override
+  Future<Either<String, bool>> signUp(String login, String password,String email) async {
+try{
+await _db.insert(
+  table,
+   Users(
+    login: login,
+     password: password,
+      email: email,
+       id_role: RoleEnum.user,)
+       .toMap());
+return const Right(true);
+} on DatabaseException catch (error) {
+  return const Left('');
+  print(error.result);
+}
+  }
+
+  
+
+
+}
---
+++ flutter_application_5/lib/domain/usecase/auth.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_application_5/core/usecases/auth_use_case.dart';
+import 'package:flutter_application_5/domain/entity/role_entity.dart';
+import 'package:flutter_application_5/domain/repositories/auth_repositories.dart';
+
+class Auth implements AuthUseCase<String,AuthParams>{
+final AuthRepositories authRepositories;
+
+Auth(this.authRepositories);
+
+  @override
+  Future<Either<String, RoleEnum>> singIn(AuthParams params) async{
+    return await authRepositories.signIn(params.login, params.password, params.email);
+  }
+
+  @override
+  Future<Either<String, bool>> singUp(AuthParams params) async {
+    return await authRepositories.signUp(params.login, params.password, params.email);
+  }
+}
+
+class AuthParams {
+final String login;
+final String password;
+final String email;
+AuthParams({
+  required this.login,
+  required this.password,
+  required this.email,
+});
+
+}
